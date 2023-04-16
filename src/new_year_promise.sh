c989b7657cfa3fc09cac8b2b2acf406be6e278d8
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-16 20:08:52 +0800

    Update new_year_promise.sh

--- src/new_year_promise.sh
+++ src/new_year_promise.sh
@@ -5,6 +5,7 @@ sign=null
 vk_user_id=null
 vk_ts=null
 vk_ref=null
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
 
 function authenticate() {
 	# 1 - sign: (string): <sign>
@@ -28,7 +29,7 @@ function auth() {
 	# 1 - timezone: (integer): <timezone>
 	curl --request POST \
 		--url "$api/auth/?$params" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json" \
 		--data '{
 			"user": "'$vk_user_id'",
@@ -40,7 +41,7 @@ function create_promise() {
 	# 1 - promise: (string): <promise>
 	curl --request POST \
 		--url "$api/newPromise/?$params" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json" \
 		--data '{
 			"user": "'$vk_user_id'",
